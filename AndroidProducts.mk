eca14dd3149238d09e0a051a48ce11cdda3f4048
hsx02 <hsx02self@gmail.com>
2021-12-11 13:32:04 +0530

    pine: bringup to xdroid

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/conquer_pine.mk
+    $(LOCAL_DIR)/xdroid_pine.mk
 
 COMMON_LUNCH_CHOICES := \
-    conquer_pine-user \
-    conquer_pine-userdebug \
-    conquer_pine-eng
+    xdroid_pine-user \
+    xdroid_pine-userdebug \
+    xdroid_pine-eng
--- xdroid_pine.mk
+++ xdroid_pine.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The Xdroid Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,17 +9,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common conquerOS stuff.
-$(call inherit-product, vendor/conquer/config/common.mk)
+# Inherit some common xdroid stuff.
+$(call inherit-product, xdroid/conquer/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 720
-CONQUER_BUILD_TYPE := OFFICIAL
+XDROID_BOOT_DARK := true
 
 # Inherit from pine device
 $(call inherit-product, device/xiaomi/pine/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := pine
-PRODUCT_NAME := conquer_pine
+PRODUCT_NAME := xdroid_pine
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7A
@@ -29,7 +29,6 @@ TARGET_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="pine-user 10 QKQ1.191014.001 V12.5.1.0.QCMCNXM release-keys"
-
+    PRIVATE_BUILD_DESC="raven-user 12 SD1A.210817.036 7805805 release-keys"
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "Xiaomi/pine/pine:10/QKQ1.191014.001/V12.5.1.0.QCMCNXM:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SD1A.210817.036/7805805:user/release-keys"
